0926c1ef007800997499324f4024d9f86817aacf
anunay <anunay@anunay>
2017-01-21 01:38:09 +0530

    Hello-world N echo

---
+++ helloWorld/src/main/java/com/deeaae/ror/helloworld/controller/BaseController.java
@@ -0,0 +1,64 @@
+
+package com.deeaae.ror.helloworld.Controllers;
+
+import com.deeaae.ror.Echo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+
+
+/**
+ * Created by [)/\ on 29/10/16.
+ */
+
+@RestController
+public class BaseController {
+
+	@Autowired
+	HttpServletRequest httpServletRequest;
+
+	private String getHeaderDetails() {
+
+		String responseText = "Welcome to Kingcross.\n" +
+				"This is where our journey starts.\n" +
+				"This application is in Spring Boot 1.4.0 with Java 8.\n" +
+				"This application is here to demonstrate Keycloak Integration and Kong Usage.\n" +
+				"This application requires you to have two roles Role_1 and Role_2 and Role_3. Sorry for my maths." +
+				" \n\n\n\n\n";
+		StringBuilder stringBuilder = new StringBuilder(responseText);
+
+		Collections
+				.list(httpServletRequest.getHeaderNames())
+				.stream()
+				.forEach(header->stringBuilder
+						.append("\n" + header + "\t" + httpServletRequest.getHeader(header) )
+				);
+		stringBuilder.append(com.deeaae.ror.Echo.echo("\n\nHowdy!!!\n\n"));
+		return  stringBuilder.toString();
+	}
+
+	@RequestMapping(value = "/echo", method = RequestMethod.GET)
+	public ResponseEntity<String> echo() {
+		return new ResponseEntity<String>(
+				getHeaderDetails(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/secured-echo", method = RequestMethod.GET)
+	public ResponseEntity<String> securedEcho() {
+
+		String responseText = "\n\t--------------Secured Page--------------\n\n"
+				+getHeaderDetails();
+		return new ResponseEntity<String>(
+				responseText, HttpStatus.OK);
+	}
+
+}
+
---
+++ project-echo/src/main/java/com/deeaae/ror/Echo.java
@@ -0,0 +1,7 @@
+package com.deeaae.ror;
+
+public class Echo {
+    public static String echo(String data) {
+        return "Echo " + data;
+    }
+}
